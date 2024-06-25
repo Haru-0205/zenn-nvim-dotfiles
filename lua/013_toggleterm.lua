caad39a73c7629f50edf04f3473c6cf87211a2a5
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-06-25 21:12:09 +0900

    add lazygit support

--- lua/013_toggleterm.lua
+++ lua/013_toggleterm.lua
@@ -5,8 +5,29 @@ local gitui = Terminal:new({
   hidden = true
 })
 
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  direction = "float",
+  hidden = true
+})
+
+local bottom_term = Terminal:new({
+  direction = "bottom_term",
+  hidden = true
+})
+
 function _gitui_toggle()
   gitui:toggle()
 end
 
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+function _bottom_term_toggle()
+  bottom_term:toggle()
+end
+
 vim.keymap.set("n", "gu", "<Cmd>lua _gitui_toggle()<CR>")
+vim.keymap.set("n", "<C-t>", "<Cmd>lua _bottom_term_toggle()<CR>")
+vim.keymap.set("n", "lg", "<Cmd>lua _lazygit_toggle()<CR>")
